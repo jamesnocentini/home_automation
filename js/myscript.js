82ef31d648cd2370eba47b255db92ac6f0955e9b
Samuel Carlisle <samuelcarlisle@gmail.com>
2012-10-24 13:00:08 +0100

    added transitions, have fun

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,9 @@
-<script type="text/javascript">
+$(document).ready(function(){
+  $("#door_off").show();
+  $("#door_on").hide();
+  $('#door1 > a').click(function(){ // For demo's sake we attach the crossFade to a click event.
+    $("#door_on").fadeToggle(500);
+    $("#door_off").fadeToggle(500); //url('../images/home_off.png') no-repeat;
+  });
+});
 
-function change() {
-document.getElementById('splash').style.background = "url(../images/door001.png) center no-repeat";
-}
