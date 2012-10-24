e0a82295bd16c5031588b84f6b8dabd504997b33
James Nocentini <james.nocentini@gmail.com>
2012-10-24 12:07:41 +0100

    first commit

---
+++ _/js/myscript.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+
+function change() {
+document.getElementById('splash').style.background = "url(../images/door0001.png) center no-repeat";
+document.getElementById('hideme').style.display = "none";
+//background: url(../images/sukeysplash.gif) center no-repeat;    
+}
